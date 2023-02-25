4331a0d8aba3ca0d381bfbdb5eec9bc9ce4ec63f
AM1k21 <105818269+AM1k21@users.noreply.github.com>
2023-02-25 19:57:07 +0100

    haters gonna hate

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+var session = require('express-session')
+
 const {
   SessionLog,
   Category,
@@ -10,6 +12,7 @@ const port = 80;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(session({ secret: "A big cat",token: null}))
 
 app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
@@ -18,43 +21,80 @@ app.use("/img", express.static(__dirname + "public/img"));
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
-app.post("/", (req, res) => {
+app.post("/main", (req, res) => {
   const name = req.body.name;
   const password = req.body.Password;
   const data = loadJSON("../json/data.json");
   const data2 = loadJSON("../json/categories.json");
   const data3 = loadJSON("../json/programmers.json");
   const LoginData = loadJSON("../json/login.json");
-  console.log(name);
-  console.log(password);
+  var remember =req.body.remember;
+if(remember == "on"){
+  remember = true;
+}else{
+  remember = false;
+}
+ const ClientID =req.body.ghost;
+
   let logged = false;
-  for (let i = 0; i < LoginData.length; i++) {
-    if (LoginData[i].name == name) {
-      if (LoginData[i].password == password) {
+  for (login of LoginData) {
+    for(save of login.saved_Devices){
+      if(save == ClientID){
+        
         logged = true;
+        req.session.token = ClientID;
+        break
       }
     }
+    if (login.name == name && login.password == password) {
+      logged = true;
+      if(remember){
+        login['saved_Devices'].push(ClientID)
+        saveJSON("../json/login.json", LoginData);
+
+      }
+    }
+    
   }
+
   if (logged == true) {
-    console.log("ssuccess!");
     res.render("pages/index", {
       data: data,
       categories: data2,
       programmers: data3,
     });
-  } else {
-    console.error("Wrong password or name");
+  } else {    res.render("pages/login", {
+    data: data,
+    categories: data2,
+    programmers: data3,
+  });
   }
 });
 app.get("/", (req, res) => {
   const data = loadJSON("../json/data.json");
   const data2 = loadJSON("../json/categories.json");
   const data3 = loadJSON("../json/programmers.json");
-  res.render("partials/login", {
+  const LoginData = loadJSON("../json/login.json");
+  let logged = false;
+  for (login of LoginData) {
+    for(save of login.saved_Devices){
+      if(save == req.session.token){
+        logged= true
+      }
+    }
+
+    
+  }
+  if(logged){        res.render("pages/index", {
     data: data,
     categories: data2,
     programmers: data3,
-  });
+
+  });}else{
+  res.render("pages/login", {
+    data: data,
+
+  });}
 });
 
 app.post("/addRecord", (req, res) => {
@@ -93,7 +133,7 @@ app.post("/login", (req, res) => {
   }
   if (logged == true) {
     console.log("ssuccess!");
-    res.render("pages/index", {
+    res.redirect("partials/login", {
       data: data,
       categories: data2,
       programmers: data3,
