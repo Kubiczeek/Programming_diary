5df55190a59404fca46e674d17b3c8bc74204ef2
Kubiczeek <79270879+Kubiczeek@users.noreply.github.com>
2023-03-31 19:22:21 +0200

    Delete public directory

--- public/js/AdminTag.js
+++
@@ -1,7 +0,0 @@
-function deleteTagQuestion(cardId) {
-  document.getElementById("overlayDiv2").style.display = "flex";
-  document.getElementById("overlayDiv2").querySelector(".textt").textContent =
-    "Are you sure you want to delete this tag?";
-  document.getElementById("cardIdNotification").value = cardId;
-  document.querySelector(".formNotificationn").action = "/admin/deleteTag";
-}
--- public/js/RecordFunctions.js
+++
@@ -1,8 +0,0 @@
-function deleteRecordQuestion(cardId) {
-  document.getElementById("overlayDiv2").style.display = "flex";
-  document.getElementById("cardIdNotification").value = cardId;
-}
-
-function cancelDeleteNotification() {
-  document.getElementById("overlayDiv2").style.display = "none";
-}
